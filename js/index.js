3d4114c1b88460158d816fe08e0debdd9e2c3860
Raudel Flores <rawdell@outlook.com>
2019-11-01 21:08:15 -0500

    created two more selectors and applied events to them. Events include scroll, drag/drop and a Loading event

--- js/index.js
+++ js/index.js
@@ -3,6 +3,8 @@
 // Selectors
 const navBar = document.querySelector('.nav-link');
 const bodyColor = document.querySelector('body');
+const bannerPhoto = document.querySelector('.intro img');
+const adventurePhoto = document.querySelector('.content-section img')
 
 // 1. Mouseover
 navBar.addEventListener('mouseover', () => {
@@ -17,4 +19,19 @@ bodyColor.addEventListener('keydown', () => {
     bodyColor.style.transition = '0.4s'
 });
 
-// 3. Drag/Drop
+// 4. Scroll
+window.addEventListener('scroll', () => {
+    adventurePhoto.style.transform = 'scale(1.4)';
+});
+
+// 4. Drag/Drop
+bannerPhoto.addEventListener('drag', (event) => {
+    bannerPhoto.style.opacity = '0.5';
+});
+
+// 5. Load
+window.addEventListener('load', () =>{
+    alert('Welcome to our page! Enjoy your stay.')
+});
+
+// 6. Focus
