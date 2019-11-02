74de35fd1a8d104cd3839babbadedeeeec7b91c4
Raudel Flores <rawdell@outlook.com>
2019-11-01 20:30:01 -0500

    created a navbar and body selector then applied two event listeners for them.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+// Selectors
+const navBar = document.querySelector('.nav-link');
+const bodyColor = document.querySelector('body');
+
+// 1. Mouseover
+navBar.addEventListener('mouseover', () => {
+    navBar.style.backgroundColor = 'black';
+    navBar.style.padding = '4px';
+    navBar.style.color = 'white';
+});
+
+// 2. Keydown
+bodyColor.addEventListener('keydown', () => {
+    bodyColor.style.backgroundColor = '#E0B39E';
+    bodyColor.style.transition = '0.4s'
+});
+
+// 3. Drag/Drop
